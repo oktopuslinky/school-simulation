3a4c9ab52ccceb98e3b44a2f7bd92bbd36a45e63
Devansh Agrawal <devagr3924@gmail.com>
2020-10-07 09:49:16 -0500

    Created classes

---
+++ 6SimulateSchool-DevanshAgrawal.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self):
+        pass
+
+class Teacher(Person):
+    def __init__(self):
+        pass
+
+class Student(Person):
+    def __init__(self):
+        pass
+
+class Menu():
+    def __init__(self):
+        pass
