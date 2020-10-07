755bc79fcce7289b5b0083ccbeb3feb21bf81c34
Devansh Agrawal <devagr3924@gmail.com>
2020-10-07 10:17:55 -0500

    Added methods and attributes

--- 6SimulateSchool-DevanshAgrawal.py
+++ 6SimulateSchool-DevanshAgrawal.py
@@ -1,15 +1,51 @@
 class Person:
     def __init__(self):
-        pass
+        self.name = ""
+
 
 class Teacher(Person):
     def __init__(self):
-        pass
+        self.students = list()
+        self.schedule = list()
 
 class Student(Person):
     def __init__(self):
+        self.teachers = list()
+        self.schedule = list()
+
+class MenuController():
+    def __init__(self):
+        self.teacher_list = list()
+        self.student_list = list()
+        self.course_list = list()
+
+    def run(self):
         pass
 
-class Menu():
-    def __init__(self):
+    def add_course(self):
+        pass
+    
+    def add_person(self):
+        #for both teachers and students
+        pass
+
+    def remove_person(self):
+        pass
+
+    def assign_course(self):
+        pass
+
+    def unassign_course(self):
+        pass
+
+    def disp_courses(self):
+        pass
+
+    def disp_action_menu(self):
+        pass
+    
+    def redirect_user(self):
+        pass
+
+    def quit(self):
         pass
